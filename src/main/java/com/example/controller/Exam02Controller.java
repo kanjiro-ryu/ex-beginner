b686906d6dc75facae902ffc1cca343617aa4cda
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2020-06-16 10:37:16 +0900

    exam02.htmlを作成

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -1,5 +1,25 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex02")
 public class Exam02Controller {
 
+	@RequestMapping("")
+	public String Index() {
+		return "exam02";
+		}
+	
+	
+	@RequestMapping("/input-form")
+	public String form(String name, Model model) {
+		
+		model.addAttribute("name", name);
+		
+		return "exam01-result";
+	}
+	
 }

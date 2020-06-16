a7a876185d1f4ad2a2cd3b980a130bb6d1dcac53
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2020-06-16 11:50:42 +0900

    Exam03Controllerを作成

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -1,5 +1,34 @@
 package com.example.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex03")
 public class Exam03Controller {
+
+	@Autowired
+	private ServletContext application;
+
+	@RequestMapping("")
+	public String Index() {
+		return "exam03";
+	}
+
+	@RequestMapping("/calcGoods")
+	public String calcGoods(Integer goods1, Integer goods2, Integer goods3) {
+		
+		int total = goods1 + goods2 + goods3;
+		int totalInclude = (int) (total * 1.10);
+		
+		
+		application.setAttribute("total", total);
+		application.setAttribute("totalInclude", totalInclude);
+		
+		return "exam03-result";
+	}
 
 }
